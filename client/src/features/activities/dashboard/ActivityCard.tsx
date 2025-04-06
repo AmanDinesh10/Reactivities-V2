27707e85fd96ab1b4d980f072ba97cfcc270eb3a
AmanDinesh <xarmaaaruti@gmail.com>
2025-04-06 11:56:20 +0530

    End of section 7

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -9,13 +9,13 @@ import {
 } from "@mui/material";
 import { Activity } from "../../../lib/types";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { Link } from "react-router";
 
 type Props = {
   activity: Activity;
-  selectActivity: (id: string) => void;
 };
 
-export default function ActivityCard({ activity, selectActivity }: Props) {
+export default function ActivityCard({ activity }: Props) {
   const { deleteActivity } = useActivities();
 
   return (
@@ -36,7 +36,8 @@ export default function ActivityCard({ activity, selectActivity }: Props) {
         <Chip label={activity.category} variant="outlined" />
         <Box display="flex" gap={3}>
           <Button
-            onClick={() => selectActivity(activity.id)}
+            component={Link}
+            to={`/activities/${activity.id}`}
             size="medium"
             variant="contained"
           >
